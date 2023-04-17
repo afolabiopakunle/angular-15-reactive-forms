7bb93e21060ebe08f0cc9680ac7f93de959c0a9e
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-17 19:41:21 +0100

    formGroup submits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+
   genders = [
     { value: 1, title: 'Male' },
     { value: 2, title: 'Female' },
     { value: 3, title: 'Undisclosed' },
   ];
+
+  signUpForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      gender: new FormControl(1)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
+    ReactiveFormsModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
